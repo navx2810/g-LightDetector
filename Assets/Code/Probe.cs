945eff644ff2ea78d299da9072975b31ff40cd34
Matt Fetterman <navx2810@gmail.com>
2015-12-03 18:27:13 -0500

    solved the issue of multiple light overlaps

--- Assets/Code/Probe.cs
+++ Assets/Code/Probe.cs
@@ -7,14 +7,24 @@ public class Probe : MonoBehaviour
 
     public bool IsInsideLight;
 
+    // This is used to keep track of the collider that first entered the object incase multiple lights try hitting the probe. The probe only needs one hit afterall
+    // This solves the problem of multiple light overlaps and when you duplicate and move the light around
+    private Collider target;
+
     public void OnTriggerExit(Collider other)
     {
+        if(target != other) { return; }
+
         IsInsideLight = false;
+        target = null;
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        if(target != null) { return; }
+
         IsInsideLight = true;
+        target = other;
     }
 
     void OnDrawGizmos()
